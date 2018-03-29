9db08b8dab7e53f24afff897422ace6d6dddb47a
Jose <jainigo@profile.es>
2018-03-30 01:32:36 +0200

    Star wars API

--- src/main/kotlin/com/codependent/cryptomarket/ui/ReactiveWorkshopApplication.kt
+++ src/main/kotlin/com/codependent/cryptomarket/ui/ReactiveWorkshopApplication.kt
@@ -2,10 +2,7 @@ package com.codependent.cryptomarket.ui
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.stream.annotation.EnableBinding
-import org.springframework.cloud.stream.messaging.Sink
 
-@EnableBinding(Sink::class)
 @SpringBootApplication
 class CryptomarketUiApplication
 
--- src/main/kotlin/com/codependent/cryptomarket/ui/web/StarWarsApiController.kt
+++ src/main/kotlin/com/codependent/cryptomarket/ui/web/StarWarsApiController.kt
@@ -1,6 +1,8 @@
 package com.codependent.cryptomarket.ui.web
 
 import com.codependent.cryptomarket.ui.dto.Character
+import org.springframework.http.MediaType.APPLICATION_JSON
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
@@ -11,9 +13,12 @@ import reactor.core.publisher.Mono
 class StarWarsApiController {
 
     @GetMapping("/api/people/{id}")
-    fun getCharacter(@PathVariable id: Int) : Mono<Character> {
+    fun getCharacter(@PathVariable id: Int): Mono<Character> {
         val webClient = WebClient.builder()
-        return webClient.baseUrl("https://swapi.co/api/").build().get().uri("/people/$id").retrieve()
+        return webClient.baseUrl("https://swapi.co/api/").build()
+                .get().uri("/people/$id")
+                .header("Content-Type", APPLICATION_JSON_VALUE)
+                .accept(APPLICATION_JSON).retrieve()
                 .bodyToMono(Character::class.java)
     }
 
