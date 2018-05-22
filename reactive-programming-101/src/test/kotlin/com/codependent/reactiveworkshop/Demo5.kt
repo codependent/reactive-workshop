e1376d450a99c6d1aa0d49a3946473b226690b99
Jose <jainigo@profile.es>
2018-05-22 14:55:48 +0200

    new tests

--- reactive-programming-101/src/test/kotlin/com/codependent/reactiveworkshop/Demo5.kt
+++ reactive-programming-101/src/test/kotlin/com/codependent/reactiveworkshop/Demo5.kt
@@ -2,8 +2,12 @@ package com.codependent.reactiveworkshop
 
 import org.junit.Assert.assertTrue
 import org.junit.Test
+import reactor.core.publisher.EmitterProcessor
 import reactor.core.publisher.Flux
+import reactor.core.publisher.ReplayProcessor
 import reactor.core.publisher.UnicastProcessor
+import reactor.core.scheduler.Schedulers
+import java.time.Duration
 import java.util.concurrent.CountDownLatch
 
 class Demo5 : DemoBase() {
@@ -133,5 +137,149 @@ class Demo5 : DemoBase() {
         latch.await()
     }
 
+    /**
+     * Suscripción a un Publisher upstream
+     * EmitterProcessor se bloquea cuando su buffer se llena
+     * El primer suscriptor recibe todos los elementos cacheados
+     * El segundo suscriptor sólo recibe los nuevos elementos
+     */
+    @Test
+    fun emitterProcessor() {
+        val latch = CountDownLatch(1)
+
+        val numbers: Flux<Int> = Flux.range(1, 1000).log()
+        val processor = EmitterProcessor.create<Int>(100)
+
+        numbers.subscribeOn(Schedulers.elastic()).subscribeWith(processor)
+
+        Thread.sleep(2000)
+
+        processor.subscribe {
+            logger.info("Subscriber1 onNext {}", it)
+        }
+
+        processor.doOnComplete {
+            logger.info("Subscriber2 onComplete()")
+            latch.countDown()
+        }.subscribe {
+            logger.info("Subscriber2 onNext() {}", it)
+        }
+
+        latch.await()
+    }
+
+    /**
+     * Cachea el último
+     */
+    @Test
+    fun replayProcessor() {
+        val latch = CountDownLatch(1)
+
+        val numbers: Flux<Int> = Flux.range(1, 1000).log()
+        val processor = ReplayProcessor.cacheLast<Int>()
+
+        numbers.subscribeOn(Schedulers.elastic()).subscribeWith(processor)
+
+        Thread.sleep(2000)
+
+        processor.subscribe {
+            logger.info("Subscriber1 onNext {}", it)
+        }
+
+        processor.doOnComplete {
+            logger.info("Subscriber2 onComplete()")
+            latch.countDown()
+        }.subscribe {
+            logger.info("Subscriber2 onNext() {}", it)
+        }
+
+        latch.await()
+    }
+
+    /**
+     * Cachea con límite de 10
+     */
+    @Test
+    fun replayProcessor2() {
+        val latch = CountDownLatch(1)
+
+        val numbers: Flux<Int> = Flux.range(1, 1000).log()
+        val processor = ReplayProcessor.create<Int>(10)
+
+        numbers.subscribeOn(Schedulers.elastic()).subscribeWith(processor)
+
+        Thread.sleep(2000)
+
+        processor.subscribe {
+            logger.info("Subscriber1 onNext {}", it)
+        }
+
+        processor.doOnComplete {
+            logger.info("Subscriber2 onComplete()")
+            latch.countDown()
+        }.subscribe {
+            logger.info("Subscriber2 onNext() {}", it)
+        }
+
+        latch.await()
+    }
+
+    /**
+     * Cachea sin límite
+     */
+    @Test
+    fun replayProcessor3() {
+        val latch = CountDownLatch(1)
+
+        val numbers: Flux<Int> = Flux.range(1, 1000).log()
+        val processor = ReplayProcessor.create<Int>()
+
+        numbers.subscribeOn(Schedulers.elastic()).subscribeWith(processor)
+
+        Thread.sleep(2000)
+
+        processor.subscribe {
+            logger.info("Subscriber1 onNext {}", it)
+        }
+
+        processor.doOnComplete {
+            logger.info("Subscriber2 onComplete()")
+            latch.countDown()
+        }.subscribe {
+            logger.info("Subscriber2 onNext() {}", it)
+        }
+
+        latch.await()
+    }
+
+    /**
+     * Cachea con ventana temporal
+     */
+    @Test
+    fun replayProcessor4() {
+        val latch = CountDownLatch(20)
+
+        val numbers: Flux<Long> = counter(100)
+        val processor = ReplayProcessor.createTimeout<Long>(Duration.ofMillis(500))
+
+        numbers.subscribeOn(Schedulers.elastic()).subscribeWith(processor)
+
+        Thread.sleep(1000)
+
+        processor.subscribe {
+            logger.info("Subscriber1 onNext {}", it)
+        }
+
+        Thread.sleep(1000)
+
+        processor.doOnComplete {
+            logger.info("Subscriber2 onComplete()")
+        }.subscribe {
+            logger.info("Subscriber2 onNext() {}", it)
+            latch.countDown()
+        }
+
+        latch.await()
+    }
 
 }
