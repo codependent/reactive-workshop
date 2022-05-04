94ee18b94ec7c590e6d2fa568c9ae1addc80005c
jainigo <jainigo@profile.es>
2022-05-04 10:11:34 +0200

    backpressure

--- reactive-programming-101/src/test/kotlin/com/codependent/reactiveworkshop/Demo6.kt
+++ reactive-programming-101/src/test/kotlin/com/codependent/reactiveworkshop/Demo6.kt
@@ -1,46 +1,48 @@
 package com.codependent.reactiveworkshop
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import reactor.core.publisher.EmitterProcessor
+import reactor.core.publisher.Sinks
+import reactor.core.publisher.Sinks.EmitResult.FAIL_ZERO_SUBSCRIBER
+import reactor.core.scheduler.Schedulers
 import java.util.concurrent.CountDownLatch
 
 class Demo6 : DemoBase() {
 
     @Test
     fun backPressureTest() {
+
         val latch = CountDownLatch(1)
 
-        val numberGenerator = counter(1)
-        val processor = EmitterProcessor.create<Long>()
-        numberGenerator.subscribeWith(processor)
+        val sink = Sinks.many().multicast().directAllOrNothing<Int>()
 
-        Thread.sleep(5000)
+        val result: Sinks.EmitResult = sink.tryEmitNext(1)
 
-        processor.doOnError {
-            latch.countDown()
-        }.subscribe {
-            logger.info("Element [{}]", it)
-        }
+        assertEquals(FAIL_ZERO_SUBSCRIBER, result)
+
+        val sink2 = Sinks.many().multicast().onBackpressureBuffer<Int>(10)
+
+        sink2.asFlux()
+            .publishOn(Schedulers.boundedElastic())
+            .subscribe {
+                logger.info("Subscriber1 {}", it)
+            }
+
+        sink2.asFlux()
+            .publishOn(Schedulers.boundedElastic())
+            .subscribe {
+                logger.info("Subscriber2 {}", it)
+                Thread.sleep(2000)
+            }
+
+        (1..1000)
+            .forEach {
+                val res = sink2.tryEmitNext(it)
+                logger.info("Result {}", res)
+                latch.countDown()
+            }
 
         latch.await()
     }
-
-    @Test
-    fun backPressure2Text() {
-        val latch = CountDownLatch(1)
-
-        val numberGenerator = counter(1)
-        val processor = EmitterProcessor.create<Long>()
-        numberGenerator.onBackpressureDrop().subscribeWith(processor)
-
-        Thread.sleep(5000)
-
-        processor.subscribe {
-            logger.info("Element [{}]", it)
-            latch.countDown()
-        }
-
-        latch.await()
-    }
-
 
 }
